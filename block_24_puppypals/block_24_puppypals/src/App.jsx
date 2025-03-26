e3bcb23f89f6658e93c8de8bc4cacec43448ecfd
Austina White <austinawhite@gmail.com>
2025-03-25 21:50:03 -0400

    first commit

--- block_24_puppypals/block_24_puppypals/src/App.jsx
+++ block_24_puppypals/block_24_puppypals/src/App.jsx
@@ -1,14 +1,28 @@
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
+import './path-to-css.css'
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    onClick={()=>{}}
+  }
 
   return (
-    <>
-     
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={handleClick} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
+
 
 export default App
